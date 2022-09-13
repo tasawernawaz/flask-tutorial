a952528febae5ed360b645775d899b5693b8f548
tasawernawaz <tasawer.nawaz@arbisoft.com>
2022-09-13 18:59:53 +0500

    A detail view to show a single post. Click a post’s title to go to its page.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -99,3 +100,9 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+
+@bp.route('/<int:id>/detail', methods=('GET',))
+def detail(id):
+    post = get_post(id, check_author=False)
+    return render_template('blog/detail.html', post=post)
